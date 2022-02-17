ba82852452043f7453879f3b1e5991a90b90fa8d
Danilo <98827421+DaniloArya@users.noreply.github.com>
2022-02-17 16:33:11 -0500

    Navbar finished

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -14,10 +14,10 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className="h-[72px] p-3 lg:p-5 lg:px-12 flex justify-between bg-gradient-to-b from-black text-white">
+    <nav className="fixed z-50 w-full h-20 p-3 lg:p-5 lg:px-12 flex justify-between bg-gradient-to-b from-black text-white proxima-nova-regular">
       <Link href="/">
         <a aria-hidden="true">
-          <div className={`relative z-10 h-full w-56 transition-all duration-500 ${effect?'translate-y-0 opacity-100':'-translate-y-20 opacity-0'}`}>
+          <div className={`relative z-10 h-full w-56 transition-all duration-700 ${effect?'translate-y-0 opacity-100':'-translate-y-20 opacity-0'}`}>
             <Image src="/logoJetour.svg" layout="fill" alt="logo Jetour" />
           </div>
         </a>
@@ -29,14 +29,14 @@ const Navbar = () => {
           <path d="M18 27H31.5V24H18V27Z" />
         </svg>
       </button>
-      <ul className={`${showMenu?'flex':'hidden'} lg:flex top-0 left-0 absolute lg:relative h-screen lg:h-full w-full bg-[#da0010] lg:bg-transparent flex-col lg:flex-row justify-center lg:justify-end items-center text-xs text-center tracking-widest uppercase proxima-nova-regular gap-8`}>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-100 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/"><a>Inicio</a></Link></li>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-200 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="#historia"><a>Historia</a></Link></li>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-300 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/modelos"><a>Modelos</a></Link></li>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-[400ms] opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/postventa"><a>Postventa</a></Link></li>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-500 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/concesionarios"><a>Red de Concecionarios</a></Link></li>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-[600ms] opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/blog"><a>Blog</a></Link></li>
-        <li className={`transition-all duration-500 ${effect?'translate-y-0 delay-700 opacity-100':'-translate-y-20 opacity-0'} py-3 px-10 bg-black lg:bg-[#da0010] `}><Link href="/cotizar"><a>Cotizar</a></Link></li>
+      <ul className={`${showMenu?'flex':'hidden'} lg:flex top-0 left-0 absolute lg:relative h-screen lg:h-full w-full gap-8 bg-[#da0010] lg:bg-transparent flex-col lg:flex-row justify-center lg:justify-end items-center text-xs text-center tracking-widest uppercase`}>
+        <li className={`transition-all duration-700 ${effect?'translate-y-0 delay-100 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/"><a>Inicio</a></Link></li>
+        <li className={`transition-all duration-700 ${effect?'translate-y-0 delay-200 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="#historia"><a>Historia</a></Link></li>
+        <li className={`transition-all duration-700 ${effect?'translate-y-0 delay-300 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/modelos"><a>Modelos</a></Link></li>
+        <li className={`transition-all duration-700 ${effect?'translate-y-0 delay-[400ms] opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/postventa"><a>Postventa</a></Link></li>
+        <li className={`transition-all duration-700 ${effect?'translate-y-0 delay-500 opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/concesionarios"><a>Red de Concecionarios</a></Link></li>
+        <li className={`transition-all duration-700 ${effect?'translate-y-0 delay-[600ms] opacity-100':'-translate-y-20 opacity-0'}`}><Link href="/blog"><a>Blog</a></Link></li>
+        <Link href="/cotizar"><a><li className={`transition-all duration-700 ${effect?'translate-y-0 delay-700 opacity-100':'-translate-y-20 opacity-0'} py-3 px-20 lg:px-10 bg-black lg:bg-[#da0010] proxima-nova-semibold`}>Cotizar</li></a></Link>
       </ul>
     </nav>
   );
