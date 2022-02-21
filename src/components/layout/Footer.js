61e7339fcbf8259c021a562b5b69b95be225dea9
Danilo <98827421+DaniloArya@users.noreply.github.com>
2022-02-21 16:10:08 -0500

    Footer with Prismic Integration

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,16 +1,30 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import { RichText } from 'prismic-reactjs'
+import htmlSerializer from '../../../lib/htmlSerializer'
 
 const Footer = ({data})=>{
-  console.log(data)
+  
   return (
     <footer className="flex flex-col p-5 gap-10 items-center bg-black text-white text-center proxima-nova-light text-xs">
       <div className="flex flex-col p-4 pt-16 gap-3 uppercase tracking-[0.2rem]">
-        <p>Jetour 2021.</p>
-        <p>TODOS LOS DERECHOS RESERVADOS.</p>
+        <RichText 
+          render={data?.data?.legal_title}
+          htmlSerializer={htmlSerializer}
+        />
       </div>
-      <p className="px-5 md:px-[15%] leading-6 tracking-widest">*Imágenes referenciales. Precio de US$17,490 o S/69,960 corresponden al modelo Jetour X70 Full MT MY2022. Tipo de cambio referencial S/ 4,00, al momento de la transacción se aplicará el TC del día. Precios no incluyen trámite de placas, tarjeta de propiedad ni flete a Provincias. El equipamiento varía por versión del modelo. Precios válidos hasta el 28/02/2022 o hasta agotar el stock disponible. Válido para Concesionarios Autorizados de la Red Altos Andes a Nivel Nacional.</p>
-      <div className="h-16 w-48 my-6 bg-slate-50">
+      <div className="px-5 md:px-[15%] leading-6 tracking-widest">
+        <RichText 
+          render={data?.data?.legal_description}
+          htmlSerializer={htmlSerializer}
+        />
       </div>
+      <Image
+        alt="Logo de jetour"
+        src={data?.data?.logo?.url}
+        width={150}
+        height={45}
+      />
       <ul className="flex flex-col lg:flex-row uppercase tracking-[0.2rem] gap-6 lg:gap-24">
         <li><Link href="/"><a>Inicio</a></Link></li>
         <li><Link href="#historia"><a>Historia</a></Link></li>
@@ -20,13 +34,22 @@ const Footer = ({data})=>{
         <li><Link href="/blog"><a>Blog</a></Link></li>
       </ul>
       <div className="w-full flex justify-between items-center">
-        <div className="hidden md:block text-left opacity-60 tracking-widest">
+        <div className="hidden md:block w-[calc(100%-192px)] text-left opacity-60 tracking-widest">
           JETOUR 2021. TODOS LOS DERECHOS RESERVADOS ・ <Link href="/politicas-privacidad"><a>PROTECCIÓN DE DATOS PERSONALES</a></Link>{'・'}<Link href="/reglamentacion-neumaticos"><a>REGLAMENTACIÓN SOBRE NEUMÁTICOS FUERA DE USO</a></Link>
         </div>
-        <div className="w-full md:w-auto flex justify-center gap-4">
-          <div className="w-10 h-10 bg-white rounded"></div>
-          <div className="w-10 h-10 bg-white rounded"></div>
-          <div className="w-10 h-10 bg-white rounded"></div>
+        <div className="w-full md:w-48 md:pr-6 flex justify-center items-center gap-10">
+          {data?.data?.social_networks_list.map((social_network, idx) => {
+            console.log(social_network?.social_network_logo?.url)
+            return (
+              <a className="flex justify-center items-center transition-transform active:-translate-y-2 md:hover:-translate-y-2" key={idx} href={social_network?.social_network_link}>
+                <Image 
+                  alt={social_network?.social_network_name}
+                  src={social_network?.social_network_logo?.url}
+                  width={social_network?.social_network_logo?.dimensions?.width * 1.5}
+                  height={social_network?.social_network_logo?.dimensions?.height * 1.5}
+                />
+              </a>)
+          })}
         </div>
       </div>
     </footer>
